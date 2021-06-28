5307a6ee602796b8cfeee70f69f616ec5e8d1a85
qystan <63985586+qystan@users.noreply.github.com>
2021-06-28 21:16:53 +0800

    Update Configuration.h
    update accel settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -913,14 +913,14 @@
  */
 #define DEFAULT_MAX_FEEDRATE \
   {                          \
-    120, 120, 5, 25          \
+    100, 80, 5, 25          \
   }
 
 #define LIMITED_MAX_FR_EDITING // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
 #define MAX_FEEDRATE_EDIT_VALUES \
   {                              \
-    240, 240, 20, 50             \
+    200, 200, 20, 50             \
   } // ...or, set your own edit limits
 #endif
 
@@ -930,9 +930,9 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION \
-  {                              \
-    1500, 1500, 500, 1000        \
+#define DEFAULT_MAX_ACCELERATION  \
+  {                               \
+    1200, 800, 500, 1000          \
   }
 
 #define LIMITED_MAX_ACCEL_EDITING // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
@@ -940,7 +940,7 @@
 #define MAX_ACCEL_EDIT_VALUES \
   {                           \
     2000, 2000, 1000, 2000    \
-  } // ...or, set your own edit limits
+  }   // ...or, set your own edit limits
 #endif
 
 /**
@@ -951,9 +951,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION 1500        // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION 900 // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION 3000 // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION 1200        // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION 600 // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION 1800 // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
@@ -993,7 +993,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-#define JUNCTION_DEVIATION_MM 0.08 // (mm) Distance from real junction edge
+#define JUNCTION_DEVIATION_MM 0.13 // (mm) Distance from real junction edge
 #define JD_HANDLE_SMALL_SEGMENTS   // Use curvature estimation instead of just the junction angle 
                                    // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
