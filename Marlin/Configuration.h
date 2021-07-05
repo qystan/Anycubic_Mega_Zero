972312b231f5fdb1a2bd7c4ae1bd831326f7b266
qystan <63985586+qystan@users.noreply.github.com>
2021-07-05 10:57:26 +0800

    Update Configuration.h
    updated defaultg_ejerk to 9.0 for cartesian printer
    junction deviation to 0.14
    run accel and junction dev test after this setting.
    set values to be refined

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -983,7 +983,7 @@
 #endif
 #endif
 
-#define DEFAULT_EJERK 5.0 // May be used by Linear Advance
+#define DEFAULT_EJERK 9.0 // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
@@ -993,7 +993,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-#define JUNCTION_DEVIATION_MM 0.13 // (mm) Distance from real junction edge
+#define JUNCTION_DEVIATION_MM 0.14 // (mm) Distance from real junction edge
 #define JD_HANDLE_SMALL_SEGMENTS   // Use curvature estimation instead of just the junction angle 
                                    // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
